1c86bf8302eec58dd48c65f37a79b1c1b55de6eb
Fawad Mahmood <50448569+FawadMahmood@users.noreply.github.com>
2025-07-29 00:20:19 +0500

    chore: added function getToken

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,7 @@ import TurboGoogleRecaptcha from './NativeTurboGoogleRecaptcha';
 export function initRecaptcha(siteKey: string): Promise<boolean> {
   return TurboGoogleRecaptcha.initRecaptcha(siteKey);
 }
+
+export function getToken(): Promise<string> {
+  return TurboGoogleRecaptcha.getToken();
+}
